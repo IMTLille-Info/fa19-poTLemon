aa3f0fb357c25305a4176a0671f96289249c770c
acourand <volugus@hotmail.fr>
2017-03-20 17:43:48 +0100

    Why

--- potlemon-client/src/main/java/Main.java
+++ potlemon-client/src/main/java/Main.java
@@ -1,7 +1,9 @@
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import potlemon.test.HelloWorld;
+import potlemon.view.FightView;
 
 public class Main {
     public static void main(String[] args) {
